9e528cf6f7c98a116b2d1a7e5e5571bcdeae8246
Boto1 <olegbotsiun@gmail.com>
2018-12-24 18:53:27 +0200

    add a feature

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
 import Button from './components/ButtonComponent';
+import Text from './components/TextComponent';
 
 class App extends Component {
+  state = {
+    counter: 0
+  };
+
+  incrementCounter = (incrementValue) => {
+    this.setState((prev) => ({
+      counter: prev.counter + incrementValue
+    }));    
+  };
+
   render() {
     return (
-      <Button />
+      <div>
+        <Button onClickFunc={this.incrementCounter.bind(this, 1)} value="1" />
+        <Button onClickFunc={this.incrementCounter.bind(this, 5)} value="5" />
+        <Button onClickFunc={this.incrementCounter.bind(this, 10)} value="10" />
+        <Button onClickFunc={this.incrementCounter.bind(this, 100)} value="100" />
+        <Text value={this.state.counter}/>
+      </div>
     );
   }
 }
--- src/components/ButtonComponent.js
+++ src/components/ButtonComponent.js
@@ -1,19 +1,11 @@
 import React, { Component } from 'react';
 
 class Button extends Component {
-  state = {
-    counter: 0
-  }
-
-  incrementCounter = () => {
-    this.setState((prev) => ({
-      counter: prev.counter + 1
-    }))
-  }
-
   render() {
     return (
-      <button onClick={this.incrementCounter}>{this.state.counter}</button>
+      <button onClick={ this.props.onClickFunc }>
+        +{ this.props.value }
+      </button>
     );
   }
 }
